8db2d48aebff6106d421fc360a915a107d90c468
Erik Spall <espall@me.com>
2019-07-08 10:22:11 +0200

    device switch commit

--- WpfControlLibrary1/UserControl1.xaml.cs
+++ WpfControlLibrary1/UserControl1.xaml.cs
@@ -21,6 +21,9 @@ namespace WpfControlLibrary1
     /// </summary>
     public partial class UserControl1 : UserControl
     {
+        List<Label> tape = new List<Label>(); //tapeArray of Labels used to Display content of TM
+        bool loaded = false;    //bool to track if everthing loaded already, so tape creation isn't messed up
+
         public UserControl1()
         {
             InitializeComponent();
@@ -28,8 +31,53 @@ namespace WpfControlLibrary1
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation dA = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(1)));
-            Test1.BeginAnimation(OpacityProperty, dA);
+            if (!loaded) {
+                loaded = true;
+                for (int i = -1; (Canvas1.ActualWidth / 50) + 2 > tape.Count; i++)
+                {
+                    Label tLabel = new Label(); //temporary Label that gets added to Array
+                    tLabel.Width = 50;
+                    tLabel.Height = 50;
+                    tLabel.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    tLabel.VerticalContentAlignment = VerticalAlignment.Center;
+                    tLabel.Content = "#";
+                    tLabel.BorderBrush = Brushes.Black;
+                    tLabel.BorderThickness = new Thickness(1);
+                    tLabel.Margin = new Thickness(i * 50, 0, 0, 0);
+                    Canvas1.Children.Add(tLabel);
+                    tape.Add(tLabel);
+                   
+                }
+               
+            }
+           /// MessageBox.Show(ActualWidth.ToString());
+            
+        }
+
+        private void UserControl_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+           // MessageBox.Show(tape.Count.ToString());
+           if ((Canvas1.ActualWidth/50)+2 > tape.Count) //Check if tape needs more Lables
+            {
+                 if (tape.Count % 2 == 0) //if even
+                {
+
+                } else //if uneven
+                {
+
+                }
+            }
+
+        }
+
+        public int currentIndex()
+        {
+            
         }
     }
 }
