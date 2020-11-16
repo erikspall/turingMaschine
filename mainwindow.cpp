7872b0368bdd59b89880506d495a5a52f1dcde1d
Erik Spall <espall@me.com>
2020-11-16 16:23:27 +0100

    design "finished"
    Implemented TabView and it's tabs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLabel *lbl = new QLabel;
+    lbl->setText("Du Hurensohn");
+    statusBar()->addWidget(lbl);
 }
 
 MainWindow::~MainWindow()
